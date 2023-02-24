464758e8c280d8fd11ea6fb4fd3649e2ee909ded
Gileard Teixeira <111284002+Gil-Ti@users.noreply.github.com>
2023-02-23 21:22:17 -0300

    Atualização app

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
+from flask import Flask, render_template, request
 import speech_recognition as sr
 import pyttsx3
+import nltk
+from nltk.chat.util import Chat, reflections
 
-# Define a linguagem do reconhecimento de voz
+app = Flask(__name__)
+pairs = [
+    # Exemplo de regra para o chatbot
+    ('oi', ['Olá!', 'Oi!', 'Tudo bem?']),
+    ('tchau', ['Tchau!', 'Até mais!', 'Foi bom falar com você.']),
+]
+chatbot = Chat(pairs, reflections)
 r = sr.Recognizer()
 
-# Configura a síntese de voz
-engine = pyttsx3.init()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def chatbot(text):
-    if "olá" in text.lower():
-        return "Olá, como posso ajudar?"
-    elif "como você está" in text.lower():
-        return "Estou bem, obrigado por perguntar. E você?"
-    else:
-        return "Desculpe, não entendi o que você falou"
+@app.route('/voice_input', methods=['POST'])
+def voice_input():
+    with sr.Microphone() as source:
+        print("Diga algo...")
+        audio = r.listen(source)
 
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    print("Fale algo: ")
-    audio = r.listen(source)
     try:
         text = r.recognize_google(audio, language='pt-BR')
-        response = chatbot(text)
+    except sr.UnknownValueError:
+        text = "Desculpe, não consegui entender o que você disse."
+    except sr.RequestError:
+        text = "Desculpe, ocorreu um erro no reconhecimento de fala."
+
+    response = chatbot.respond(text)
+
+    with sr.Microphone() as source:
         print(response)
+        engine = pyttsx3.init()
         engine.say(response)
         engine.runAndWait()
-    except:
-        print("Desculpe, não entendi o que você falou")
+
+    return render_template('index.html', text=text, response=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
